b55108a8c3e928115574408c909b9c0fe4f85fba
Greg Woodward <woodward@users.noreply.github.com>
2024-12-10 19:01:12 -0800

    Add license and other boilerplate for publishing

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule Statics.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/woodward/statics"
+
   def project do
     [
       app: :statics,
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Statics in Elixir",
+      package: package()
     ]
   end
 
@@ -20,6 +24,21 @@ defmodule Statics.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.35", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Greg Woodward"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url
+      },
+      files: [
+        "lib",
+        "mix.exs",
+        "LICENSE.md"
+      ]
     ]
   end
 end
